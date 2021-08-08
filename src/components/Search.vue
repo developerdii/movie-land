a3a7803b1ac4cf1069840e400e9cd4be9bbea17c
emetin <emetin@iperasolutions.com>
2021-08-08 22:16:14 +0300

    search tabs

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,19 +5,60 @@
       <input type="text" v-model="searchText" @click="getTrendingData" class="form-control pl-5"
              placeholder="Search..."/>
     </div>
-    <div v-if="isShownSearchList" @click.passive="test" id="search-list" class="list-group position-fixed mr-2 mt-3" style="right: 0">
+    <div v-if="isShownSearchList" id="search-list" class="list-group position-fixed mr-2 mt-3 bg-light shadow-sm"
+         style="right: 0;">
+      <nav id="search-nav-bar" class="nav nav-pills flex-column flex-sm-row">
+        <a v-for="media in mediaTypes" :key="media.id" class="flex-sm-fill text-sm-center nav-link shadow m-1"
+           :class="defaultMediaType.id === media.id ? 'active' : ''" aria-current="page"
+           @click="selectMediaType(media)">
+          {{ media.text }}</a>
+      </nav>
+
       <a href="#" v-for="item in searchList" :key="item.id"
          class="list-group-item list-group-item-action flex-column align-items-start">
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">{{ item.title }}</h5>
-          <small>{{ item.release_date }}</small>
-        </div>
-        <div class="row mt-1" style="width: 40rem">
-          <div class="col-md-2 pr-0">
-            <img style="width: 70%;" :src="'https://image.tmdb.org/t/p/w500'+item.poster_path">
+        <div v-if="item.media_type === 'movie'">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{ item.title }}</h5>
+            <small>{{ item.release_date }}</small>
           </div>
-          <div class="col-md-10 pl-0">
-            <small class="search-list-description"> {{ item.overview }}</small>
+          <div class="row mt-1" style="width: 40rem">
+            <div class="col-md-2 pr-0">
+              <img style="width: 70%;" :src="'https://image.tmdb.org/t/p/w500'+item.poster_path">
+            </div>
+            <div class="col-md-10 pl-0">
+              <small class="search-list-description"> {{ item.overview }}</small>
+            </div>
+          </div>
+        </div>
+        <div v-if="item.media_type === 'tv'">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{ item.name }}</h5>
+            <small>{{ item.first_air_date }}</small>
+          </div>
+          <div class="row mt-1" style="width: 40rem">
+            <div class="col-md-2 pr-0">
+              <img style="width: 70%;" :src="'https://image.tmdb.org/t/p/w500'+item.poster_path">
+            </div>
+            <div class="col-md-10 pl-0">
+              <small class="search-list-description"> {{ item.overview }}</small>
+            </div>
+          </div>
+        </div>
+        <div v-if="item.media_type === 'person'">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{ item.name }}</h5>
+            <small>Popularity: <span class="text-info">{{ item.popularity }}</span></small>
+          </div>
+          <div class="row mt-1" style="width: 40rem">
+            <div class="col-md-2 pr-0">
+              <img style="width: 70%;" :src="'https://image.tmdb.org/t/p/w500'+item.profile_path">
+            </div>
+            <div class="col-md-10 pl-0">
+              <p v-for="(movie,index) in item.known_for" :key="index" class="font-weight-light m-1">
+                <a class="font-weight-light" href="">{{ movie.title + ' - ' }}</a>
+                <i class="fas fa-star" style="color: orange"></i> {{ movie.vote_average }}
+              </p>
+            </div>
           </div>
         </div>
       </a>
@@ -33,6 +74,28 @@ export default {
     return {
       searchText: '',
       searchList: [],
+      defaultMediaType: {
+        id: 1,
+        type: 'movie',
+        text: 'Movie'
+      },
+      mediaTypes: [
+        {
+          id: 1,
+          type: 'movie',
+          text: 'Movie'
+        },
+        {
+          id: 2,
+          type: 'tv',
+          text: 'TV'
+        },
+        {
+          id: 3,
+          type: 'person',
+          text: 'Person'
+        },
+      ],
       isShownSearchList: false,
     }
   },
@@ -44,10 +107,15 @@ export default {
     }
   },
   methods: {
-    async getTrendingData() {
+    async getTrendingData(media = this.defaultMediaType) {
       this.isShownSearchList = true;
-      let response = await api.getTrendingMovies('all', 'week')
+      let response = await api.getTrendingMovies(this.defaultMediaType.type, 'day')
       this.searchList = response.data.results.slice(0, 5)
+    },
+
+    selectMediaType(media) {
+      this.defaultMediaType = media;
+      this.getTrendingData()
     },
 
     search() {
@@ -57,7 +125,9 @@ export default {
     hideList(event) {
       let listElement = document.getElementById('search-list')
       let searchBox = document.querySelector('#search input')
-      if (event.target !== searchBox && event.target !== listElement) {
+      let searchNavBar = document.querySelector('#search-nav-bar')
+      if (event.target !== searchBox && event.target !== listElement && event.srcElement.parentElement !== searchNavBar) {
+        console.log(event, searchNavBar)
         this.isShownSearchList = false;
       }
     }
