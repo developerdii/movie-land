7b555cd071b47f62124bce056373a046f07da963
emetin <emetin@iperasolutions.com>
2021-08-10 02:13:21 +0300

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,13 @@
 <template>
   <nav id="nav" class="navbar navbar-expand-lg navbar-light shadow-sm bg-light">
     <div class="container-xxl">
-      <a class="navbar-brand" href="#"><img class="w-50" src="../assets/logo.svg" alt=""></a>
+      <router-link class="navbar-brand" to="/"><img class="w-50" src="../assets/logo.svg" alt=""></router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
               aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarScroll">
         <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" href="#" to="/link">Link</router-link>
-          </li>
-
 
           <!--            <li class="nav-item dropdown">-->
           <!--              <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">-->
