0e98f0bfd3e96904c3fcdf82fa7a6c5aa24d10fc
emetin <emetin@iperasolutions.com>
2021-08-14 18:17:53 +0300

    css

--- src/mixins/dateFormatter.js
+++ src/mixins/dateFormatter.js
@@ -3,6 +3,7 @@ import moment from "moment";
 export default {
     methods: {
         getFormattedDate(date,format) {
+            if (date === null || date === undefined || date === '') return ''
             return moment(date).format(format)
         },
     }
