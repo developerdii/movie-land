72a855a83df7006d55852dca361d9a68cede63f4
emetin <emetin@iperasolutions.com>
2021-08-14 17:36:51 +0300

    slider

--- src/components/PopularMoviesSlider.vue
+++ src/components/PopularMoviesSlider.vue
@@ -39,11 +39,11 @@
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="carousel-control-prev-icon p-3" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="carousel-control-next-icon p-3" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
   </div>
--- src/components/UpComing.vue
+++ src/components/UpComing.vue
@@ -5,7 +5,7 @@
       <div class="carousel-inner">
         <div class="carousel-item" :class="index === 0 ? 'active' : ''" v-for="(row,index) in upComingMovies"
              :key="index" data-bs-interval="300033">
-          <div class="row p-2">
+          <div class="row p-3">
             <div class="col-md-2 p-1 d-flex" v-for="(movie,movieIndex) in row" :key="movieIndex">
               <div class="movie-card">
                 <img class="card-img-top" :src="'https://image.tmdb.org/t/p/w500'+movie.poster_path"
@@ -24,12 +24,12 @@
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2"
               data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="carousel-control-prev-icon p-3" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </button>
       <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators2"
               data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="carousel-control-next-icon p-3" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
     </div>
@@ -61,6 +61,7 @@ export default {
 <style lang="scss">
 $movie-card-radius: 1.2rem;
 .movie-card {
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   color: #fff;
   background: linear-gradient(0deg, #273b4f 0%, #34495e 100%);
   border-radius: $movie-card-radius $movie-card-radius calc(#{$movie-card-radius} / 2) calc(#{$movie-card-radius} / 2);
@@ -69,19 +70,20 @@ $movie-card-radius: 1.2rem;
   flex-direction: column;
 
   img {
-    border-radius: calc(#{$movie-card-radius} - 0.2rem) calc(#{$movie-card-radius} - 0.2rem) 0 0;
+    border-radius: calc(#{$movie-card-radius} - 0.2rem) calc(#{$movie-card-radius} - 0.3rem) 0 0;
     -webkit-mask-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, black), color-stop(0.35, black), color-stop(0.5, black), color-stop(0.65, black), color-stop(0.85, rgba(0, 0, 0, 0.6)), color-stop(1, transparent));
   }
 
   .description {
-    transition: transform .5s ease;
-    min-height: 4rem;
+    transition: transform 1.5s ease;
+    min-height: 5.5rem;
     color: lightcyan;
     text-transform: uppercase;
   }
 
   .description:hover {
     transform: scale(1.1);
+    color: #0c63e4;
   }
 
   .footer {
@@ -101,6 +103,19 @@ $movie-card-radius: 1.2rem;
   clear: both;
   color: #1B1B1B;
   border-top: 0.2rem solid #351C75;
+  font-family: Calibri, sans-serif;
+}
+
+#carouselExampleIndicators2 {
+  .carousel-control-prev {
+    background: radial-gradient(circle closest-side, rgba(0,0,0,.1), transparent);
+    margin: 10rem 0;
+  }
+  .carousel-control-next {
+    background: radial-gradient(circle closest-side, rgba(0,0,0,.1), transparent);
+    border-radius: 10px;
+    margin: 10rem 0;
+  }
 }
 
 
