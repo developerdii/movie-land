6e6da3898c906988c6d2c16de56c22f6a334d31d
emetin <emetin@iperasolutions.com>
2021-11-02 01:32:20 +0300

    nprogress

--- src/main.js
+++ src/main.js
@@ -4,10 +4,26 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import moment from "moment";
+import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css';
 
 Vue.config.productionTip = false
 
 Vue.use(moment)
+
+NProgress.configure({ parent: '#app' });
+NProgress.configure({ showSpinner: false });
+
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  return config
+})
+
+axios.interceptors.response.use(response => {
+  NProgress.done()
+  return response
+})
 
 new Vue({
   router,
