1d7a29e7927aac2c888f6fbbff4717b4255097fd
emetin <emetin@iperasolutions.com>
2021-11-21 16:03:28 +0300

    cast

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -21,56 +21,76 @@
             <img :src="'https://image.tmdb.org/t/p/w500'+movieData.poster_path" class="rounded">
           </div>
           <div class="col-md-7">
-          <div v-if="selectedTab === Enums.SLIDER">
+            <div v-if="selectedTab === Enums.SLIDER">
             <span v-for="tags in movieData.genres" :key="tags.id" class="badge bg-secondary mr-1 mb-2">
              <h6 class="mb-0">{{ tags.name }}</h6>
           </span>
-            <p class="lead mt-5">
-              {{ movieData.overview }}
-            </p>
-            <p v-if="movieData.tagline" class="mt-5 blockquote-footer font-italic" style="font-size: 1rem">
-              {{ movieData.tagline }}</p>
-          </div>
+              <p class="lead mt-5">
+                {{ movieData.overview }}
+              </p>
+              <p v-if="movieData.tagline" class="mt-5 blockquote-footer font-italic" style="font-size: 1rem">
+                {{ movieData.tagline }}</p>
+            </div>
             <div class="column-footer">
               <ul class="nav justify-content-center">
                 <li class="nav-item">
                   <a class="nav-link nav-icon" :class="selectedTab === Enums.SLIDER ? 'text-primary' : ''"
-                     href="javascript:void();" @click="selectViewTab(Enums.SLIDER)">
+                     href="javascript:void(0);" @click="selectViewTab(Enums.SLIDER)">
                     <i class="fa fa-th-large"></i>
                   </a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link nav-icon" :class="selectedTab === Enums.LIST ? 'text-primary' : ''"
-                     href="javascript:void();" @click="selectViewTab(Enums.LIST)">
+                     href="javascript:void(0);" @click="selectViewTab(Enums.LIST)">
                     <i class="fa fa-bars"></i>
                   </a>
                 </li>
               </ul>
-              <slider v-if="selectedTab === Enums.SLIDER">
-                <div slot="card">
-                  <div class="carousel-item" :class="index === 0 ? 'active' : ''" v-for="(row,index) in cast"
-                       :key="index" data-bs-interval="300033">
-                    <div class="row pr-3 pl-3">
-                      <div class="col-md-3 p-1 d-flex" v-for="(person,personIndex) in row" :key="personIndex">
-                        <div class="movie-card">
-                          <img class="card-img-top" :src="'https://image.tmdb.org/t/p/w500'+person.profile_path">
-                          <p class="p-2 text-center mb-0 pb-0">
-                            {{ person.name }}
-                          </p>
-                          <div class="p-2 text-center footer" style="min-height: 60px">
-                            {{ person.character }}
+              <div class="row">
+                <slider v-if="selectedTab === Enums.SLIDER">
+                  <div slot="card">
+                    <div class="carousel-item" :class="index === 0 ? 'active' : ''" v-for="(row,index) in cast"
+                         :key="index" data-bs-interval="300033">
+                      <div class="row pr-3 pl-3">
+                        <div class="col-md-3 p-1 d-flex" v-for="(person,personIndex) in row" :key="personIndex">
+                          <div class="movie-card">
+                            <img class="card-img-top" :src="'https://image.tmdb.org/t/p/w500'+person.profile_path">
+                            <p class="p-2 text-center mb-0 pb-0">
+                              {{ person.name }}
+                            </p>
+                            <div class="p-2 text-center footer" style="min-height: 60px">
+                              {{ person.character }}
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </slider>
-              <div v-if="selectedTab === Enums.LIST">
-                <div class="row">
-                    <div class="list-group col-md-6" v-for="listGroup in cast" :key="listGroup">
-                      <li class="list-group-item" v-for="person in listGroup" :key="person">{{ person.name }}</li>
+                </slider>
+                <div v-if="selectedTab === Enums.LIST">
+                  <div style="min-height: 60vh">
+                    <div class="row border rounded bg-white" style="width: 45rem;" v-for="(person, index) in cast[page]" :key="index">
+                      <div class="col-md-2 pl-0">
+                        <img class="rounded-left" style="width: 60%;" :src="'https://image.tmdb.org/t/p/w500'+person.profile_path">
+                      </div>
+                      <div class="col-md-10">
+                        <p class="lead"><strong>{{ person.name }}</strong></p>
+                        <p class="text-muted">{{ person.character }}</p>
+                      </div>
                     </div>
+                  </div>
+                  <div class="pt-2">
+                    <ul class="pagination float-right mr-5 mb-0" style="font-size: 1.5rem">
+                      <li @click="changePage(true)"><a :class="disableNext ? 'disable-paginate' : ''" href="javascript:void(0);">
+                        <i class="fas fa-chevron-right"></i>
+                      </a></li>
+                    </ul>
+                    <ul class="pagination float-left mb-0" style="font-size: 1.5rem">
+                      <li @click="changePage(false)"><a :class="disablePrevious ? 'disable-paginate' : ''" href="javascript:void(0);">
+                        <i class="fas fa-chevron-left"></i>
+                      </a></li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
@@ -110,7 +130,8 @@ export default {
       recommendations: [],
       loading: false,
       selectedTab: Enums.SLIDER,
-      Enums
+      Enums,
+      page: 0,
     }
   },
   watch: {
@@ -126,7 +147,16 @@ export default {
     },
 
     castViewSize() {
-      return this.selectedTab === Enums.SLIDER ? 4 : 12;
+      return this.selectedTab === Enums.SLIDER ? 4 : 6;
+    },
+
+    disablePrevious() {
+      return this.page <= 0;
+    },
+
+    disableNext() {
+      let pageSize = this.cast.length;
+      return this.page >= pageSize - 1;
     }
   },
 
@@ -183,9 +213,19 @@ export default {
 
     selectViewTab(id) {
       this.selectedTab = id;
+      this.page = 0;
       this.chunkCredits(this.fullCastData);
-    }
+    },
 
+    changePage(isNext) {
+      if (isNext) {
+        if (this.disableNext) return
+        this.page++
+      } else {
+        if (this.disablePrevious) return
+        this.page--;
+      }
+    }
   }
 
 }
@@ -205,5 +245,11 @@ export default {
 
 .active {
   color: red;
+}
+
+.disable-paginate {
+  color: #6c757d;
+  pointer-events: none;
+  cursor: default;
 }
 </style>
