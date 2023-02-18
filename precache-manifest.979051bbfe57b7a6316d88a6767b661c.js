bfad96a0ef1b3e2a63f09d7a15853d448ae90345
emetin <emetin@iperasolutions.com>
2023-02-18 18:21:20 +0300

    new deployment

---
+++ precache-manifest.979051bbfe57b7a6316d88a6767b661c.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa7c83cd60d3c3be9ae",
+    "url": "css/app.0d6b90a7.css"
+  },
+  {
+    "revision": "c5872cb21ce793be8a25",
+    "url": "css/chunk-62014531.f2ffb18a.css"
+  },
+  {
+    "revision": "dcccc312ec1c2f19d6c2",
+    "url": "css/chunk-vendors.902ebb66.css"
+  },
+  {
+    "revision": "6502e62f9e1d9b0d0a3755b217de8c98",
+    "url": "img/logo.6502e62f.svg"
+  },
+  {
+    "revision": "ba41d9bddeb151263a43556fbaccd602",
+    "url": "index.html"
+  },
+  {
+    "revision": "caa7c83cd60d3c3be9ae",
+    "url": "js/app.9d90204a.js"
+  },
+  {
+    "revision": "c5872cb21ce793be8a25",
+    "url": "js/chunk-62014531.be0eb29c.js"
+  },
+  {
+    "revision": "dcccc312ec1c2f19d6c2",
+    "url": "js/chunk-vendors.a591705f.js"
+  },
+  {
+    "revision": "d24f5ebabb7a8d22c848579f607c8e90",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "6e2d21c4bb5a16188f5b492084fdedcb",
+    "url": "static/bootstrap/bootstrap.min.css"
+  },
+  {
+    "revision": "65278447595c3d1496ae89ea33039163",
+    "url": "static/bootstrap/bootstrap.min.js"
+  },
+  {
+    "revision": "74c4b86c353791a9b6e4e1d06bdc0384",
+    "url": "static/bootstrap/sb.min.css"
+  },
+  {
+    "revision": "5a6c200378fa114299418364d6382279",
+    "url": "static/fontawesome/css/all.min.css"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "static/fontawesome/webfonts/fa-brands-400.eot"
+  },
+  {
+    "revision": "cafa7a5ee0a9964b4839d1e533a3f9c8",
+    "url": "static/fontawesome/webfonts/fa-brands-400.svg"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "static/fontawesome/webfonts/fa-brands-400.ttf"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "static/fontawesome/webfonts/fa-brands-400.woff"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "static/fontawesome/webfonts/fa-brands-400.woff2"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "static/fontawesome/webfonts/fa-regular-400.eot"
+  },
+  {
+    "revision": "99902ff08653f1e230f5b3a19f272c20",
+    "url": "static/fontawesome/webfonts/fa-regular-400.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "static/fontawesome/webfonts/fa-regular-400.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "static/fontawesome/webfonts/fa-regular-400.woff"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "static/fontawesome/webfonts/fa-regular-400.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "static/fontawesome/webfonts/fa-solid-900.eot"
+  },
+  {
+    "revision": "85224f3327dd8ce404193fb7e9685d72",
+    "url": "static/fontawesome/webfonts/fa-solid-900.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "static/fontawesome/webfonts/fa-solid-900.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "static/fontawesome/webfonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "static/fontawesome/webfonts/fa-solid-900.woff2"
+  }
+]);
